3ea4fb534352122ff809cc674d8c023645dc1794
zaid halim <zaid_abd.alhalim@yahoo.com>
2021-04-07 01:06:19 +0300

    adding the fees implementation plus some enhancements

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,6 @@ const NftRegistry = artifacts.require("NftRegistry");
 module.exports = async function (deployer) {
   const accounts = await web3.eth.getAccounts()
   const feeAccount = accounts [0]
-  const feePercent = 10
 
-  await deployer.deploy(NftRegistry,feeAccount , feePercent);
+  await deployer.deploy(NftRegistry,feeAccount);
 };
--- src/contracts/NftRegistry.sol
+++ src/contracts/NftRegistry.sol
@@ -1,15 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.8.4;
-
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./NftFactory.sol";
 
 contract NftRegistry is NftFactory {
-  
+  using SafeMath for uint256;
 
-  constructor(address _feeAccount, uint256 _feePercent) public {
+  constructor(address _feeAccount) public {
     feeAccount = _feeAccount;
-    feePercent = _feePercent;
   }
+
+  /*
+   * when you deploy Cifi_Token to Local testNet (Ganache) or to testnet like binance test net take the address of the
+   * deployed Cifi_Token and add it below
+   * the address below is just a dummy one that we need to change once we deploy the Cifi_Token Contract to the
+   * binance main net and take the address of the contract and add it below
+   * but for testing purposes just deploy the cifiToken to Ganache and take the address and use it.
+   */
+
+  ERC20 cifiTokenContract = ERC20(0x135a6636C8d1D5099C6a6A1D8D0762C4c9Ed9f09);
+  uint256 constant FEE = 10;
 
   event RegistryCreated(
     string name,
@@ -22,7 +33,7 @@ contract NftRegistry is NftFactory {
     string memory name,
     string memory symbol,
     string memory description
-  ) public  returns (bool) {
+  ) public returns (bool) {
     require(msg.sender != address(0), "Invalid address");
     require(bytes(name).length != 0, "name can't be empty");
     require(bytes(symbol).length != 0, "symbol can't be empty");
@@ -30,10 +41,22 @@ contract NftRegistry is NftFactory {
       keccak256(bytes(registries[symbol].symbol)) != keccak256(bytes(symbol)),
       "symbol is already taken"
     );
+    uint256 balaceOfUser = cifiTokenContract.balanceOf(msg.sender);
+    uint8 cifiDecimals = cifiTokenContract.decimals();
+    uint256 feeAmount = FEE.mul(10**cifiDecimals).div(100);
+    require(
+      balaceOfUser >= feeAmount,
+      "insufficient  Balance, unable to pay the registration Fee"
+    );
 
+    cifiTokenContract.transferFrom(msg.sender, feeAccount, feeAmount);
     Registry(name, symbol, description, msg.sender);
 
     return true;
+  }
+
+  function balance1() public view returns (uint256) {
+    return cifiTokenContract.balanceOf(msg.sender);
   }
 
   function getRegistryAddress(string memory symbol)
